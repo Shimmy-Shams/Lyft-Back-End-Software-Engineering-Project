e04f55206a59eef8f1f3add68e8652a60deb252c
Shimmy-Shams <19srs12@queensu.ca>
2023-02-08 03:10:47 -0500

    Refactoring the code as per the second task.

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,16 @@
+from battery.battery import Battery
+
+
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+        self.nubbin_battery_needs_service_year = 3
+
+    def needs_service(self):
+        date_need_service = self.last_service_date.replace(
+            year=self.last_service_date + self.nubbin_battery_needs_service_year)
+        if date_need_service < self.current_date:
+            return True
+        else:
+            return False
